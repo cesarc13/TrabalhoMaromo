f0592aeb6f149f08bee652a1a05fee348d3bf52a
cesarc13 <86213874+cesarc13@users.noreply.github.com>
2021-12-05 22:14:32 -0300

    Add files via upload

---
+++ exercícios.c.c
@@ -0,0 +1,58 @@
+#include<stdio.h>
+/*
+int main () {
+    //exercício 01
+    int idade;
+    scanf ("&d", &idade);
+    if (idade>=18){}
+        printf("R$15,00");
+        } else
+        printf("RS10,00");
+        }
+*/
+/*
+        //exercício 02
+        int idade;
+        scanf("%d", %idade);
+        printf("%s", (idade>=18? "R$15,00" : "10,00"));
+        */
+/*
+        //exercício 03
+        int num1, num2, res;
+        printf("digite o primeiro numero:");
+        scanf("%d, &num1)");
+        printf("digite o segundo numero:");
+        scanf("%d", &num2");
+        printf("digite o operador");
+        scanf("digite o operador");
+        fflush(stdin);
+        scanf("%c", &operador");
+        if(operador=='+') {
+           res = num1, num2,;
+        printf("%d %c %d=%d", num1, operador, num2, res);
+        }
+        else if (operador=='-');{
+             res = nume1 + num2;
+        }
+        else if (operador=='*' || operador=='x'){
+        res = num1 * num2;
+        else if (operador=='/')
+             res = num1 / num2;
+        else
+            printf("Operador invalido");
+        }
+*/
+
+      // exercício 04      {
+
+
+      int num1, num2, res;
+      char operador;
+      printf ("digite o primeiro numero>:");
+      scanf ("%d", &num1);
+      printf (digite o segundo numero:);
+      scanf ("#d, num2");
+      printf ("digite o operador");
+      fflush (stdin);
+      scanf ("%c", %operador);
+
